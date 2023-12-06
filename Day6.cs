2272232394fd953d721007d07a4d49b8162d1333
Dan Burgener <dan.burgener@gmail.com>
2023-12-05 22:21:19 -0700

    Day Six

---
+++ Day6.cs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode2023
+{
+    public static class Day6
+    {
+        public static async Task<long> One()
+        {
+            var data = await Common.ReadFile("Six", "One");
+            var timeRow = data[0];
+            var distanceRow = data[01];
+
+            List<int> times = timeRow.Split(":")[1].Trim().Split(" ").Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
+            List<int> distances = distanceRow.Split(":")[1].Trim().Split(" ").Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => int.Parse(x)).ToList();
+            int total = 0;
+            for(int i = 0; i < times.Count; i++)
+            {
+                var waysToWinThisGame = GetWaysToWin(times[i], distances[i]);
+                if (total == 0)
+                {
+                    total = waysToWinThisGame;
+                }
+                else
+                {
+                    total *= waysToWinThisGame;
+                }
+            }
+            return total;
+        }
+
+        private static int GetWaysToWin(int raceTime, int recordDistance)
+        {
+            int waysToWin = 0;
+            for(int timePressed = 1; timePressed < raceTime; timePressed++)
+            {
+                int totalDistance = timePressed * (raceTime - timePressed);
+                if (totalDistance > recordDistance)
+                {
+                    waysToWin++;
+                }
+            }
+            return waysToWin;
+        }
+        private static long GetWaysToWin(long raceTime, long recordDistance)
+        {
+            long waysToWin = 0;
+            for (long timePressed = 1; timePressed < raceTime; timePressed++)
+            {
+                long totalDistance = timePressed * (raceTime - timePressed);
+                if (totalDistance > recordDistance)
+                {
+                    waysToWin++;
+                }
+            }
+            return waysToWin;
+        }
+
+        public static async Task<long> Two()
+        {
+            var data = await Common.ReadFile("Six", "Two");
+            var timeRow = data[0];
+            var distanceRow = data[01];
+
+            long time = long.Parse(string.Join("", timeRow.Split(":")[1].Trim().Split(" ").Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x)));
+            long distance = long.Parse(string.Join("", distanceRow.Split(":")[1].Trim().Split(" ").Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x)));
+            return GetWaysToWin(time, distance);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,25 @@
 ﻿using AdventOfCode2023;
 
-Console.WriteLine("Day 1");
-Console.WriteLine($"{nameof(Day1.One)}: {await Day1.One()}");
-Console.WriteLine($"{nameof(Day1.Two)}: {await Day1.Two()}");
+//Console.WriteLine("Day 1");
+//Console.WriteLine($"{nameof(Day1.One)}: {await Day1.One()}");
+//Console.WriteLine($"{nameof(Day1.Two)}: {await Day1.Two()}");
 
-Console.WriteLine("Day 2");
-Console.WriteLine($"{nameof(Day2.One)}: {await Day2.One()}");
-Console.WriteLine($"{nameof(Day2.Two)}: {await Day2.Two()}");
+//Console.WriteLine("Day 2");
+//Console.WriteLine($"{nameof(Day2.One)}: {await Day2.One()}");
+//Console.WriteLine($"{nameof(Day2.Two)}: {await Day2.Two()}");
 
-Console.WriteLine("Day 3");
-Console.WriteLine($"{nameof(Day3.One)}: {await Day3.One()}");
-Console.WriteLine($"{nameof(Day3.Two)}: {await Day3.Two()}");
+//Console.WriteLine("Day 3");
+//Console.WriteLine($"{nameof(Day3.One)}: {await Day3.One()}");
+//Console.WriteLine($"{nameof(Day3.Two)}: {await Day3.Two()}");
 
-Console.WriteLine("Day 4");
-Console.WriteLine($"{nameof(Day4.One)}: {await Day4.One()}");
-Console.WriteLine($"{nameof(Day4.Two)}: {await Day4.Two()}");
+//Console.WriteLine("Day 4");
+//Console.WriteLine($"{nameof(Day4.One)}: {await Day4.One()}");
+//Console.WriteLine($"{nameof(Day4.Two)}: {await Day4.Two()}");
 
-Console.WriteLine("Day 5");
-Console.WriteLine($"{nameof(Day5.One)}: {await Day5.One()}");
-Console.WriteLine($"{nameof(Day5.Two)}: {await Day5.Two()}");
+//Console.WriteLine("Day 5");
+//Console.WriteLine($"{nameof(Day5.One)}: {await Day5.One()}");
+//Console.WriteLine($"{nameof(Day5.Two)}: {await Day5.Two()}");
+
+Console.WriteLine("Day 6");
+Console.WriteLine($"{nameof(Day6.One)}: {await Day6.One()}");
+Console.WriteLine($"{nameof(Day6.Two)}: {await Day6.Two()}");

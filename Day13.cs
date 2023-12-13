f0da0e055c2598adfddf2d3e2f41ba0b54e2898a
Dan Burgener <dan.burgener@gmail.com>
2023-12-13 01:10:44 -0700

    Day 13 Part 2 Complete

--- Day13.cs
+++ Day13.cs
@@ -17,14 +17,20 @@
 
         private static int FindReflection(Pattern pattern)
         {
-            int horizontal = FindReflectionV2(pattern.Horizontals);
-            int vertical = FindReflectionV2(pattern.Verticals);
+            int horizontal = FindReflection(pattern.Horizontals);
+            int vertical = FindReflection(pattern.Verticals);
             return (horizontal * 100) + vertical;
         }
 
-        private static int FindReflectionV2(List<string> lines)
+        private static int FindReflectionWithSmudge(Pattern pattern)
         {
-            bool even = lines.Count % 2 == 0;
+            int horizontalWithSmudge = FindReflectionWithSmudge(pattern.Horizontals);
+            int verticalWithSmudge = FindReflectionWithSmudge(pattern.Verticals);
+            return (horizontalWithSmudge * 100) + verticalWithSmudge;
+        }
+
+        private static int FindReflection(List<string> lines)
+        {
             int indexToCheck = 0;
             do
             {
@@ -44,6 +50,43 @@
                 indexToCheck++;
             } while (indexToCheck < lines.Count-1);
             return 0;
+        }
+
+        private static int FindReflectionWithSmudge(List<string> lines)
+        {
+            int indexToCheck = 0;
+            do
+            {
+                bool perfectReflection = true;
+                bool smudged = false;
+                for (var i = 0; i + indexToCheck < lines.Count - 1 && indexToCheck - i >= 0; i++)
+                {
+                    if (lines[indexToCheck - i] != lines[indexToCheck + i + 1])
+                    {
+                        if (!smudged && MightHaveSmudge(lines[indexToCheck - i], lines[indexToCheck + i + 1]))
+                        {
+                            smudged = true;
+                        }
+                        else
+                        {
+                            perfectReflection = false;
+                            break;
+                        }
+                    }
+                }
+                if (smudged && perfectReflection)
+                {
+                    return indexToCheck + 1;
+                }
+                indexToCheck++;
+            } while (indexToCheck < lines.Count - 1);
+            return 0;
+        }
+
+        private static bool MightHaveSmudge(string a, string b)
+        {
+            return a.Zip(b, (x, y) => x == y).Count(z => !z)
+           + Math.Abs(a.Length - b.Length) == 1;
         }
 
         private static List<Pattern> GeneratePatterns(string[] data)
@@ -76,7 +119,12 @@
         public static async Task<long> Two()
         {
             var data = await Common.ReadFile("Thirteen", "Two");
+            List<Pattern> patterns = GeneratePatterns(data);
             var sum = 0;
+            foreach (var pattern in patterns)
+            {
+                sum += FindReflectionWithSmudge(pattern);
+            }
 
             return sum;
         }
